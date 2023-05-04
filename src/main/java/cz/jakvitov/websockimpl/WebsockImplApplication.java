cf3971a2155c79ce0b4dd75b9f7a838bfc9a63c4
Jakub Vítovec <jakvitov@gmail.com>
2023-05-04 23:22:31 +0200

    Initial commit

---
+++ src/main/java/cz/jakvitov/websockimpl/WebsockImplApplication.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.websockimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsockImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/jakvitov/websockimpl/WebsockImplApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.jakvitov.websockimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
